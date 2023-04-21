e8d7231da5acc797ffaa095cd6091b2e09989d66
Siustis Teodor <siustis.teodor@gmail.com>
2023-04-21 21:42:46 +0300

    Added code for bot to connect to server and simple command

--- MultiPurposeBot/Program.cs
+++ MultiPurposeBot/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+
+namespace csharpi
+{
+    class Program
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly IConfiguration _config;
+
+        public static Task Main(string[] args) => new Program().MainAsync();
+
+        public async Task MainAsync(string[] args)
+        {
+
+        }
+
+        public Program()
+        {
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            _client.Ready += Ready;
+
+            _client.MessageReceived += MessageReceivedAsync;
+
+            var _builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile(path: "config.json");
+            _config = _builder.Build();
+        }
+
+        public async Task MainAsync()
+        {
+            await _client.LoginAsync(TokenType.Bot, _config["Token"]);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+            return Task.CompletedTask;
+        }
+
+        private Task Ready()
+        {
+            Console.WriteLine($"Connected as -> [] :)");
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(SocketMessage message)
+        {
+            if (message.Author.Id == _client.CurrentUser.Id)
+                return;
+
+            if (message.Content == ".hello")
+            {
+                await message.Channel.SendMessageAsync("world!");
+            }
+        }
+    }
+}
